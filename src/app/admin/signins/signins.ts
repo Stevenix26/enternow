78da6b1b99fa2ea2566d17d750c087491601d142
Stevenix <Stephenix26@gmail.com>
2025-10-28 12:45:14 +0100

    feat: Implement login, report, signins, and staff modules with corresponding components and styles
    - Added login component with form validation and styling.
    - Created report component with a simple template.
    - Developed signins component with a table to display staff sign-in data.
    - Implemented staff module with face capture, sign-in, and sign-out components.
    - Integrated Angular Material for UI components and styling.
    - Established routing for admin and staff modules.
    - Added custom theme for Angular Material components.
    - Created unit tests for all new components to ensure functionality.

---
+++ src/app/admin/signins/signins.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+export interface StaffElement {
+  name: string;
+  department: string;
+  count: string;
+  signin: boolean;
+  staffId: string;
+}
+
+const staffData: StaffElement[] = [
+  {
+    name: 'Tope',
+    department: 'Administration',
+    count: '4/5',
+    signin: true,
+    staffId: 'Staff ID 1023',
+  },
+  { name: 'Sayo', department: 'Sales', count: '1/5', signin: true, staffId: 'Staff ID 1023' },
+  { name: 'Segun', department: 'Production', count: '2/5', signin: true, staffId: 'Staff ID 1023' },
+  {
+    name: 'Able',
+    department: 'Human Resources',
+    count: '4/5',
+    signin: true,
+    staffId: 'Staff ID 1023',
+  },
+  { name: 'Janet', department: 'IT', count: '3/5', signin: true, staffId: 'Staff ID 1023' },
+];
+@Component({
+  selector: 'app-signins',
+  standalone: false,
+  templateUrl: './signins.html',
+  styleUrl: './signins.css',
+})
+export class Signins {
+  // column identifiers must match the matColumnDef values in the template
+  displayedColumns: string[] = ['s/n', 'name', 'department', 'signin', 'staffId'];
+  dataSource = staffData;
+}
---
+++ src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './components/header/header';
+import { MaterialModule } from './material/material-module';
+import { ReactiveFormsModule} from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    App,
+    Header
+  ],
+  imports: [
+    BrowserModule, //import the browser module
+    AppRoutingModule, //import the app routing module
+    MaterialModule, //import the material module
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners() //provide global error listeners for the browser
+  ],
+  bootstrap: [App] //bootstrap the app component
+})
+export class AppModule { }
